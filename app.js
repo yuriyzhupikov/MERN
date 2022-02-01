0b3192ec2b028b1ed000838c261398a775af28cb
Yurii <webdev56@gmail.com>
2022-02-01 09:53:42 +0300

    added connection with mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURL'), {
+
+        });
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
 
 app.listen(PORT, () => console.log(`App has been started von port ${PORT}`));
