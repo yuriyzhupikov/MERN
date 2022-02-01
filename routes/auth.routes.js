7f55f5439564a06eb73d2d3eef668c88fb945593
Yurii <webdev56@gmail.com>
2022-02-01 10:15:43 +0300

    added router handler for registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,12 @@
 const {Router} = require('express');
 const router = Router();
 
-console.log("Hello");
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+    } catch (e) {
+        res.status(500).json({messaage: 'Error'});
+    }
+})
 
 module.exports = router;
