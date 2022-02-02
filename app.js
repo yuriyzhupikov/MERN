12301a1f5f843773a17f28e70ea8feb526cd29ad
Yurii <webdev56@gmail.com>
2022-02-02 22:21:35 +0300

    fixed

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 
-const router = require("./routes/auth.routes");
-
 const app = express();
 const PORT = config.get('port') || 5000;
 
 app.use(express.json({extended: true}));
-app.use('/api/auth', router);
+app.use('/api/auth', require("./routes/auth.routes"));
 
 async function start() {
     try {
