565eb9db28f720a62643a85815d5c71814ae40c0
Yurii <webdev56@gmail.com>
2022-02-03 18:25:00 +0300

    added frontend for the CreatePage

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/Auth.context";
+import {useNavigate} from "react-router-dom";
 
 export const CreatePage = () => {
+    const navigate = useNavigate();
+    const auth = useContext(AuthContext);
+    const [link, setLink] = useState('');
+    const {request} = useHttp();
+
+    useEffect(() => {
+        window.M.updateTextFields();
+    },[]);
+
+    const pressHandler =async event => {
+        if (event.key === 'Enter') {
+            try {
+                const data = await request('/api/link/generate', 'POST', {from: link}, {authorization: `Bearer ${auth.token}`});
+                navigate(`/detail/${data.link._id}`);
+            } catch (e) {}
+        }
+    }
+
     return (
-        <div>
-            <h1>CreatePage</h1>
+        <div className="row">
+            <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
+                <div className="input-field">
+                    <input
+                        placeholder="Вставьте ссылку"
+                        id="link"
+                        type="text"
+                        onChange={e => setLink(e.target.value)}
+                        onKeyPress={pressHandler}
+                    />
+                    <label htmlFor="link">Введите ссылку</label>
+                </div>
+            </div>
         </div>
     );
 }
