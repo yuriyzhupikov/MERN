442e9384f5768806d5ea8b38d2d70a15c9d3eed3
Yurii <webdev56@gmail.com>
2022-02-08 10:53:42 +0300

    changed the DetailPage component for getting a link

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {useHttp} from "../hooks/http.hook";
+import {Loader} from "../components/Loader";
+import {LinkDescription} from "../components/LinkDescription";
+import {AuthContext} from "../context/Auth.context";
 
 export const DetailPage = () => {
+    const token = useContext(AuthContext).token;
+    const {request, loading} = useHttp();
+    const [link, setLink] = useState(null);
+    const linkId = useParams().id;
+
+    const getLink = useCallback(async () => {
+        try {
+            const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+                Authorization: `Bearer ${token}`
+            });
+            setLink(fetched);
+        } catch (e) {}
+    },[token, linkId, request]);
+
+    useEffect(() => {
+        let p = getLink();
+    }, [getLink]);
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
         <div>
-            <h1>DetailPage</h1>
+            {!loading && link && <LinkDescription linkId={link}/>}
         </div>
     );
 }
