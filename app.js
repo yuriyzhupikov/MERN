9439ca7c059ac5b39580b302c0ddd3b6bdef4902
Yurii <webdev56@gmail.com>
2022-02-09 10:49:18 +0300

    added processing of static files in production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const PORT = config.get('port') || 5000;
@@ -9,6 +10,13 @@ app.use(express.json({extended: true}));
 app.use('/api/auth', require("./routes/auth.routes"));
 app.use('/api/link', require("./routes/link.routes"));
 app.use('/to', require('./routes/redirect.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 async function start() {
     try {
